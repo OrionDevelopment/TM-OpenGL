d4616933ac6237f63c390fddcb0a0892668f017d
Orion <oriondevelopment@outlook.com>
2016-03-05 20:02:29 +0100

    Added initial dependencies and Main classes.

---
+++ src/com/smithsgaming/transportmanager/client/render/Display.java
@@ -0,0 +1,17 @@
+package com.smithsgaming.transportmanager.client.render;
+
+import org.lwjgl.system.*;
+
+/**
+ * Created by Marc on 05.03.2016.
+ */
+public class Display
+{
+
+    public Display(){
+        Configuration.LIBRARY_PATH.set("/target/natives");
+
+
+    }
+
+}
---
+++ src/com/smithsgaming/transportmanager/main/TransportManager.java
@@ -0,0 +1,7 @@
+package com.smithsgaming.transportmanager.main;
+
+/**
+ * Created by Marc on 05.03.2016.
+ */
+public class TransportManager {
+}
