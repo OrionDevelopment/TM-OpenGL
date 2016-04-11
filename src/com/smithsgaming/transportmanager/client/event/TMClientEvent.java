6535fdcf349b1aa0bd7b113163a4a3067d6e3344
Orion <oriondevelopment@outlook.com>
2016-04-11 17:54:53 +0200

    Fix for the Networking problems.

--- src/com/smithsgaming/transportmanager/client/event/TMClientEvent.java
+++ src/com/smithsgaming/transportmanager/client/event/TMClientEvent.java
@@ -1,9 +1,11 @@
 package com.smithsgaming.transportmanager.client.event;
 
-import com.smithsgaming.transportmanager.util.event.TMEvent;
+import com.smithsgaming.transportmanager.util.event.*;
+
+import java.io.*;
 
 /**
  * Created by Tim on 11/04/2016.
  */
-public abstract class TMClientEvent extends TMEvent {
+public abstract class TMClientEvent extends TMEvent implements Serializable {
 }
